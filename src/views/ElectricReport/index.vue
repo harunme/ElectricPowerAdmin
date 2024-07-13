29b593eb72eb8737a0219190f3a99d4686f6a23b
harunme <yowangbin@gmail.com>
2024-07-13 12:52:41 +0800

    feat: 电力运行报表

--- src/views/ElectricReport/index.vue
+++ src/views/ElectricReport/index.vue
@@ -1,63 +1,121 @@
 <template>
-  <div class="flex-column">
+  <div class="ElectricReport flex-column">
     <TransformerSelect />
     <div class="main-box">
       <div class="card left-box">
         <CircuitInfoTree />
       </div>
-      <div class="card table-box flex-column">
+      <div class="card flex-column">
         <el-form :inline="true" :model="formInline" class="table-form-inline">
           <el-form-item label="日期">
-            <el-date-picker v-model="formInline.date" type="date" placeholder="Pick a month" />
+            <el-date-picker v-model="formInline.starttime" type="date" @change="changeStartTime" />
           </el-form-item>
           <el-form-item label="电压类别">
-            <el-select v-model="formInline.type">
-              <el-option label="全部" value="shanghai" />
-              <el-option label="相电压" value="beijing" />
-              <el-option label="线电压" value="beijing" />
+            <el-select v-model="formInline.voltageType" @change="changeVoltageType">
+              <el-option label="全部" value="all" />
+              <el-option label="相电压" value="pv" />
+              <el-option label="线电压" value="lv" />
             </el-select>
           </el-form-item>
           <el-form-item label="时间间隔">
-            <el-select v-model="formInline.time">
-              <el-option label="一分钟" value="1min" />
-              <el-option label="五分钟" value="5min" />
-              <el-option label="十五分钟" value="15min" />
-              <el-option label="半小时" value="30min" />
-              <el-option label="一小时" value="60min" />
+            <el-select v-model="formInline.timeinterval">
+              <el-option label="一分钟" value="1" />
+              <el-option label="五分钟" value="5" />
+              <el-option label="十五分钟" value="15" />
+              <el-option label="半小时" value="30" />
+              <el-option label="一小时" value="60" />
             </el-select>
           </el-form-item>
           <el-form-item>
             <el-button-group>
-              <el-button>
+              <el-button type="primary" @click="clickPrev">
                 <el-icon class="el-icon--left"><ArrowLeft /></el-icon>上一日
               </el-button>
-              <el-button>
+              <el-button type="primary" @click="clickNext">
                 下一日<el-icon class="el-icon--right"><ArrowRight /></el-icon>
               </el-button>
             </el-button-group>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="onSubmit">查询</el-button>
-            <el-button type="primary">导出</el-button>
+            <el-button>导出</el-button>
           </el-form-item>
         </el-form>
-        <PaginationTable :span-method="objectSpanMethod" :columns="columns" :fetch-data="fetchData"> </PaginationTable>
+        <PaginationTable ref="tableRef" :span-method="objectSpanMethod" :columns="columns" :fetch-data="fetchData">
+        </PaginationTable>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="tsx" name="bing">
-import { reactive } from "vue";
+import { ref, reactive } from "vue";
+import moment from "moment";
+import { ElectricReport } from "@/api/modules/main";
 import PaginationTable, { SpanMethodProps } from "@/components/PaginationTable/index.vue";
 import TransformerSelect from "@/components/TransformerSelect/index.vue";
 import CircuitInfoTree from "@/components/CircuitInfoTree/index.vue";
 
-const formInline = reactive({
-  type: "shanghai",
-  date: "",
-  time: "1min"
+const tableRef = ref<any>(null);
+
+const formInline = reactive<{
+  starttime: string;
+  timeinterval: string;
+  voltageType: "all" | "pv" | "lv";
+}>({
+  timeinterval: "1",
+  starttime: "2024-06-01",
+  voltageType: "all"
 });
+
+const AllColumns = [
+  { prop: "circuitname", label: "回路名称" },
+  { prop: "collecttime", label: "采集时间", width: 200 },
+  { prop: "fEpi", label: "Epi(kWh)", width: 92 },
+  { prop: "fUa", label: "Ua(V)" },
+  { prop: "fUb", label: "Ub(V)" },
+  { prop: "fUc", label: "Uc(V)" },
+  { prop: "fUab", label: "Uab(V)" },
+  { prop: "fUbc", label: "Ubc(V)" },
+  { prop: "fUca", label: "Uca(V)" },
+  { prop: "fIa", label: "Ia(A)" },
+  { prop: "fIb", label: "Ib(A)" },
+  { prop: "fIc", label: "Ic(A)" },
+  { prop: "fP", label: "P(kW)" },
+  { prop: "fQ", label: "Q(kVar)" },
+  { prop: "fPF", label: "Pf" }
+];
+
+const PvColumns = [
+  { prop: "circuitname", label: "回路名称" },
+  { prop: "collecttime", label: "采集时间", width: 200 },
+  { prop: "fEpi", label: "Epi(kWh)", width: 92 },
+  { prop: "fUa", label: "Ua(V)" },
+  { prop: "fUb", label: "Ub(V)" },
+  { prop: "fUc", label: "Uc(V)" },
+  { prop: "fIa", label: "Ia(A)" },
+  { prop: "fIb", label: "Ib(A)" },
+  { prop: "fIc", label: "Ic(A)" },
+  { prop: "fP", label: "P(kW)" },
+  { prop: "fQ", label: "Q(kVar)" },
+  { prop: "fPF", label: "Pf" }
+];
+
+const LvColumns = [
+  { prop: "circuitname", label: "回路名称" },
+  { prop: "collecttime", label: "采集时间", width: 200 },
+  { prop: "fEpi", label: "Epi(kWh)", width: 92 },
+  { prop: "fUab", label: "Uab(V)" },
+  { prop: "fUbc", label: "Ubc(V)" },
+  { prop: "fUca", label: "Uca(V)" },
+  { prop: "fIa", label: "Ia(A)" },
+  { prop: "fIb", label: "Ib(A)" },
+  { prop: "fIc", label: "Ic(A)" },
+  { prop: "fP", label: "P(kW)" },
+  { prop: "fQ", label: "Q(kVar)" },
+  { prop: "fPF", label: "Pf" }
+];
+
+const columns = ref(AllColumns);
 
 const objectSpanMethod = ({ rowIndex, columnIndex }: SpanMethodProps) => {
   if (columnIndex === 0) {
@@ -75,28 +133,46 @@ const objectSpanMethod = ({ rowIndex, columnIndex }: SpanMethodProps) => {
   }
 };
 
-const columns = [
-  { prop: "fSubname", label: "回路名称" },
-  { prop: "fTransname", label: "采集时间" },
-  { prop: "transformername", label: "Epi(kWh)" },
-  { prop: "transformername", label: "Ua(V)" },
-  { prop: "transformername", label: "Ub(V)" },
-  { prop: "transformername", label: "Uc(V)" },
-  { prop: "transformername", label: "Ia(A)" },
-  { prop: "transformername", label: "Ib(A)" },
-  { prop: "transformername", label: "Ic(A)" },
-  { prop: "transformername", label: "P(kW)" },
-  { prop: "transformername", label: "Q(kVar)" },
-  { prop: "transformername", label: "Pf" }
-];
+const clickPrev = () => {
+  formInline.starttime = moment(formInline.starttime).subtract(1, "d").format("YYYY-MM-DD");
+  tableRef?.value?.resetData();
+};
 
-const onSubmit = () => {
-  console.log("submit!");
+const clickNext = () => {
+  formInline.starttime = moment(formInline.starttime).add(1, "d").format("YYYY-MM-DD");
+  tableRef?.value?.resetData();
+};
+
+const changeStartTime = value => {
+  formInline.starttime = moment(value).format("YYYY-MM-DD");
+  tableRef?.value?.resetData();
+};
+
+const changeVoltageType = value => {
+  console.log("changeVoltageType", value);
+  if (value === "pv") {
+    columns.value = PvColumns;
+  }
+  if (value === "lv") {
+    columns.value = LvColumns;
+  }
+  if (value === "all") {
+    columns.value = AllColumns;
+  }
 };
 
 const fetchData = async (): Promise<any> => {
   return new Promise(async resolve => {
-    resolve({ list: [], total: 0 });
+    const params = {
+      stationid: "000",
+      circuitid: "000",
+      starttime: formInline.starttime,
+      timeinterval: formInline.timeinterval
+    };
+    const { data } = await ElectricReport(params);
+    const list = data?.PowerValue || [];
+
+    resolve({ list, total: 0 });
   });
 };
 </script>
