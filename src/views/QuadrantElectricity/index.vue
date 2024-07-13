a35ed7472dd33b36e893d3f7201459166fbf9b9c
harunme <yowangbin@gmail.com>
2024-07-13 11:58:38 +0800

    feat: 平均功率因数、四象限电能

--- src/views/QuadrantElectricity/index.vue
+++ src/views/QuadrantElectricity/index.vue
@@ -3,55 +3,70 @@
     <TransformerSelect />
     <div class="main-box">
       <div class="card left-box"></div>
-      <div class="card table-box flex-column">
+      <div class="card flex-column">
         <el-form :inline="true" :model="formInline" class="table-form-inline">
           <el-form-item label="报表类型">
-            <el-select v-model="formInline.type" placeholder="Activity zone" clearable>
-              <el-option label="日报" value="yuebao" />
-              <el-option label="月报" value="yuebao" />
-              <el-option label="年报" value="beijing" />
+            <el-select v-model="formInline.scheme">
+              <el-option label="月报" value="M" />
+              <el-option label="年报" value="Y" />
             </el-select>
           </el-form-item>
           <el-form-item label="日期">
-            <el-date-picker v-model="formInline.date" type="date" placeholder="Pick a month" />
+            <el-date-picker v-model="formInline.starttime" :type="formInline.scheme === 'M' ? 'month' : 'year'" />
           </el-form-item>
           <el-form-item>
-            <el-button-group>
-              <el-button>
+            <el-button-group v-if="formInline.scheme === 'M'">
+              <el-button @click="clickPrev">
                 <el-icon class="el-icon--left"><ArrowLeft /></el-icon>上一月
               </el-button>
-              <el-button>
+              <el-button @click="clickNext">
                 下一月<el-icon class="el-icon--right"><ArrowRight /></el-icon>
+              </el-button>
+            </el-button-group>
+            <el-button-group v-else>
+              <el-button @click="clickPrev">
+                <el-icon class="el-icon--left"><ArrowLeft /></el-icon>上一年
+              </el-button>
+              <el-button @click="clickNext">
+                下一年<el-icon class="el-icon--right"><ArrowRight /></el-icon>
               </el-button>
             </el-button-group>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="onSubmit">查询</el-button>
-            <el-button type="primary">导出</el-button>
+            <el-button>导出</el-button>
           </el-form-item>
         </el-form>
         <div class="chart-box">
-          <ECharts :option="option" />
+          <ECharts v-if="option !== null" :option="option" />
         </div>
-        <PaginationTable :span-method="objectSpanMethod" :columns="columns" :fetch-data="fetchData"> </PaginationTable>
+        <PaginationTable ref="tableRef" :span-method="objectSpanMethod" :columns="columns" :fetch-data="fetchData">
+        </PaginationTable>
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="tsx" name="bing">
-import { reactive } from "vue";
+<script setup lang="tsx" name="QuadrantElectricity">
+import { ref, reactive } from "vue";
+import { min, max } from "lodash";
+import moment from "moment";
+import { AveragePowerReport } from "@/api/modules/main";
 import PaginationTable, { SpanMethodProps } from "@/components/PaginationTable/index.vue";
 import TransformerSelect from "@/components/TransformerSelect/index.vue";
-import { ECOption } from "@/components/Charts/config";
+// import { ECOption } from "@/components/Charts/config";
 import ECharts from "@/components/Charts/echarts.vue";
-import data from "./data.json";
+// import data from "./data.json";
 
-const formInline = reactive({
-  type: "yuebao",
-  date: "",
-  time: "1min"
+const formInline = reactive<{
+  scheme: "M" | "Y";
+  starttime: string;
+}>({
+  scheme: "M",
+  starttime: moment().format("YYYY-MM")
 });
+
+const tableRef = ref<any>(null);
+const option = ref<any>(null);
 
 const objectSpanMethod = ({ rowIndex, columnIndex }: SpanMethodProps) => {
   if (columnIndex === 0) {
@@ -69,110 +84,152 @@ const objectSpanMethod = ({ rowIndex, columnIndex }: SpanMethodProps) => {
   }
 };
 
-const columns = [
-  { prop: "fSubname", label: "回路名称" },
-  { prop: "fTransname", label: "日期" },
-  { prop: "transformername", label: "正向有功电度" },
-  { prop: "transformername", label: "反向有功电度" },
-  { prop: "transformername", label: "正向无功电度" },
-  { prop: "transformername", label: "反向无功电度" }
-];
-
-const onSubmit = () => {
-  console.log("submit!");
+const clickPrev = () => {
+  if (formInline.scheme === "M") formInline.starttime = moment(formInline.starttime).subtract(1, "M").format("YYYY-MM");
+  else formInline.starttime = moment(formInline.starttime).subtract(1, "y").format("YYYY");
+  tableRef?.value?.resetData();
 };
+
+const clickNext = () => {
+  if (formInline.scheme === "M") formInline.starttime = moment(formInline.starttime).add(1, "M").format("YYYY-MM");
+  if (formInline.scheme === "Y") formInline.starttime = moment(formInline.starttime).add(1, "y").format("YYYY");
+  tableRef?.value?.resetData();
+};
+
+const columns = [
+  { prop: "circuitname", label: "回路名称" },
+  { prop: "collecttime", label: "日期" },
+  { prop: "fEpe", label: "正向有功电度" },
+  { prop: "fEpi", label: "反向有功电度" },
+  { prop: "fEqc", label: "正向无功电度" },
+  { prop: "fEql", label: "反向无功电度" },
+  { prop: "fPf", label: "平均功率因数" }
+];
 
 const fetchData = async (): Promise<any> => {
   return new Promise(async resolve => {
-    resolve({ list: [], total: 0 });
+    const params = {
+      stationid: "000",
+      circuitid: "000",
+      scheme: formInline.scheme,
+      starttime: formInline.starttime
+    };
+    const { data } = await AveragePowerReport(params);
+    const list =
+      data?.list.map(i => ({
+        ...i,
+        fPf: Number(i.fPf) / 1000
+      })) || [];
+    option.value = {
+      grid: {
+        left: 10,
+        right: 10,
+        bottom: 10,
+        top: 64,
+        containLabel: true
+      },
+      tooltip: {
+        trigger: "axis",
+        axisPointer: {
+          type: "cross",
+          crossStyle: {
+            color: "#999"
+          }
+        }
+      },
+      toolbox: {
+        feature: {
+          dataView: { show: true, readOnly: false },
+          magicType: { show: true, type: ["line", "bar"] },
+          restore: { show: true },
+          saveAsImage: { show: true }
+        }
+      },
+      legend: {
+        data: ["正向有功电度", "反向有功电度", "正向无功电度", "反向无功电度", "功率因数"]
+      },
+      xAxis: [
+        {
+          type: "category",
+          data: list.map(({ collecttime }) => collecttime),
+          axisPointer: {
+            type: "shadow"
+          }
+        }
+      ],
+      yAxis: [
+        {
+          type: "value",
+          name: "有功电度/无功电度"
+        },
+        {
+          type: "value",
+          name: "功率因数",
+          min: min(list.map(({ pf }) => pf)),
+          max: max(list.map(({ pf }) => pf))
+        }
+      ],
+      series: [
+        {
+          name: "正向有功电度",
+          type: "bar",
+          tooltip: {
+            valueFormatter: function (value) {
+              return value + " ml";
+            }
+          },
+          data: list.map(({ fEpe }) => Number(fEpe))
+        },
+        {
+          name: "反向有功电度",
+          type: "bar",
+          tooltip: {
+            valueFormatter: function (value) {
+              return value + " ml";
+            }
+          },
+          data: list.map(({ fEpi }) => Number(fEpi))
+        },
+        {
+          name: "正向无功电度",
+          type: "bar",
+          tooltip: {
+            valueFormatter: function (value) {
+              return value + " ml";
+            }
+          },
+          data: list.map(({ fEqc }) => Number(fEqc))
+        },
+        {
+          name: "反向无功电度",
+          type: "bar",
+          tooltip: {
+            valueFormatter: function (value) {
+              return value + " ml";
+            }
+          },
+          data: list.map(({ fEql }) => Number(fEql))
+        },
+        {
+          name: "功率因数",
+          type: "line",
+          yAxisIndex: 1,
+          smooth: 0.6,
+          lineStyle: {
+            color: "#ffba00",
+            width: 3
+          },
+          tooltip: {
+            valueFormatter: function (value) {
+              return value + " °C";
+            }
+          },
+          data: list.map(({ fPf }) => Number(fPf))
+        }
+      ]
+    };
+    resolve({ list });
   });
-};
-
-const option: ECOption = {
-  grid: {
-    left: 10,
-    right: 10,
-    bottom: 10,
-    top: 64,
-    containLabel: true
-  },
-  tooltip: {
-    trigger: "axis",
-    axisPointer: {
-      type: "cross",
-      crossStyle: {
-        color: "#999"
-      }
-    }
-  },
-  toolbox: {
-    feature: {
-      dataView: { show: true, readOnly: false },
-      magicType: { show: true, type: ["line", "bar"] },
-      restore: { show: true },
-      saveAsImage: { show: true }
-    }
-  },
-  legend: {
-    data: ["正向有功电度", "反向有功电度", "正向无功电度", "反向无功电度"]
-  },
-  xAxis: [
-    {
-      type: "category",
-      data: data.data.list.map(({ f_Date }) => f_Date),
-      axisPointer: {
-        type: "shadow"
-      }
-    }
-  ],
-  yAxis: [
-    {
-      type: "value",
-      name: "有功电度/无功电度"
-    }
-  ],
-  series: [
-    {
-      name: "正向有功电度",
-      type: "bar",
-      tooltip: {
-        valueFormatter: function (value) {
-          return value + " ml";
-        }
-      },
-      data: data.data.list.map(({ epi }) => epi)
-    },
-    {
-      name: "反向有功电度",
-      type: "bar",
-      tooltip: {
-        valueFormatter: function (value) {
-          return value + " ml";
-        }
-      },
-      data: data.data.list.map(({ epe }) => epe)
-    },
-    {
-      name: "正向无功电度",
-      type: "bar",
-      tooltip: {
-        valueFormatter: function (value) {
-          return value + " ml";
-        }
-      },
-      data: data.data.list.map(({ eql }) => eql)
-    },
-    {
-      name: "反向无功电度",
-      type: "bar",
-      tooltip: {
-        valueFormatter: function (value) {
-          return value + " ml";
-        }
-      },
-      data: data.data.list.map(({ eqc }) => eqc)
-    }
-  ]
 };
 </script>
 
