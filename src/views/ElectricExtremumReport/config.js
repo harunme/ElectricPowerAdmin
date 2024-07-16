92cdc66e45fc7507a129c60dd600e537c8b89230
harunme <yowangbin@gmail.com>
2024-07-16 21:01:16 +0800

    feat: 电力极值报表

---
+++ src/views/ElectricExtremumReport/config.js
@@ -0,0 +1,452 @@
+const columns = {
+  I: [
+    { prop: "objectname", label: "回路名称" },
+    { prop: "collecttime", label: "日期", width: 200 },
+    {
+      label: "A相电流(A)",
+      children: [
+        {
+          label: "最大值",
+          children: [
+            { prop: "fIamaxvalue", label: "数值" },
+            { prop: "fIamaxtime", label: "发生时间" }
+          ]
+        },
+        {
+          label: "最小值",
+          children: [
+            { prop: "fIaminvalue", label: "数值" },
+            { prop: "fIamintime", label: "发生时间" }
+          ]
+        },
+        { prop: "fIaavg", label: "平均值" }
+      ]
+    },
+    {
+      label: "B相电流(A)",
+      children: [
+        {
+          label: "最大值",
+          children: [
+            { prop: "fIbmaxvalue", label: "数值" },
+            { prop: "fIbmaxtime", label: "发生时间" }
+          ]
+        },
+        {
+          label: "最小值",
+          children: [
+            { prop: "fIbminvalue", label: "数值" },
+            { prop: "fIbmintime", label: "发生时间" }
+          ]
+        },
+        { prop: "fIbavg", label: "平均值" }
+      ]
+    },
+    {
+      label: "C相电流(A)",
+      children: [
+        {
+          label: "最大值",
+          children: [
+            { prop: "fIcmaxvalue", label: "数值" },
+            { prop: "fIcmaxtime", label: "发生时间" }
+          ]
+        },
+        {
+          label: "最小值",
+          children: [
+            { prop: "fIcminvalue", label: "数值" },
+            { prop: "fIcmintime", label: "发生时间" }
+          ]
+        },
+        { prop: "fIcavg", label: "平均值" }
+      ]
+    }
+  ],
+  U: [
+    { prop: "objectname", label: "回路名称" },
+    { prop: "collecttime", label: "日期", width: 200 },
+    {
+      label: "A相电压(V)",
+      children: [
+        {
+          label: "最大值",
+          children: [
+            { prop: "fUamaxvalue", label: "数值" },
+            { prop: "fUamaxtime", label: "发生时间" }
+          ]
+        },
+        {
+          label: "最小值",
+          children: [
+            { prop: "fUaminvalue", label: "数值" },
+            { prop: "fUamintime", label: "发生时间" }
+          ]
+        },
+        { prop: "fUaavg", label: "平均值" }
+      ]
+    },
+    {
+      label: "B相电压(V)",
+      children: [
+        {
+          label: "最大值",
+          children: [
+            { prop: "fUbmaxvalue", label: "数值" },
+            { prop: "fUbmaxtime", label: "发生时间" }
+          ]
+        },
+        {
+          label: "最小值",
+          children: [
+            { prop: "fUbminvalue", label: "数值" },
+            { prop: "fUbmintime", label: "发生时间" }
+          ]
+        },
+        { prop: "fUbavg", label: "平均值" }
+      ]
+    },
+    {
+      label: "C相电压(V)",
+      children: [
+        {
+          label: "最大值",
+          children: [
+            { prop: "fUcmaxvalue", label: "数值" },
+            { prop: "fUcmaxtime", label: "发生时间" }
+          ]
+        },
+        {
+          label: "最小值",
+          children: [
+            { prop: "fUcminvalue", label: "数值" },
+            { prop: "fUcmintime", label: "发生时间" }
+          ]
+        },
+        { prop: "fUcavg", label: "平均值" }
+      ]
+    }
+  ],
+  ABCU: [
+    { prop: "objectname", label: "回路名称" },
+    { prop: "collecttime", label: "日期", width: 200 },
+    {
+      label: "AB线电压(V)",
+      children: [
+        {
+          label: "最大值",
+          children: [
+            { prop: "fUabmaxvalue", label: "数值" },
+            { prop: "fUabmaxtime", label: "发生时间" }
+          ]
+        },
+        {
+          label: "最小值",
+          children: [
+            { prop: "fUabminvalue", label: "数值" },
+            { prop: "fUabmintime", label: "发生时间" }
+          ]
+        },
+        { prop: "fUabavg", label: "平均值" }
+      ]
+    },
+    {
+      label: "BC线电压(V)",
+      children: [
+        {
+          label: "最大值",
+          children: [
+            { prop: "fUbcmaxvalue", label: "数值" },
+            { prop: "fUbcmaxtime", label: "发生时间" }
+          ]
+        },
+        {
+          label: "最小值",
+          children: [
+            { prop: "fUbcminvalue", label: "数值" },
+            { prop: "fUbcmintime", label: "发生时间" }
+          ]
+        },
+        { prop: "fUbcavg", label: "平均值" }
+      ]
+    },
+    {
+      label: "CA线电压(V)",
+      children: [
+        {
+          label: "最大值",
+          children: [
+            { prop: "fUcamaxvalue", label: "数值" },
+            { prop: "fUcamaxtime", label: "发生时间" }
+          ]
+        },
+        {
+          label: "最小值",
+          children: [
+            { prop: "fUcaminvalue", label: "数值" },
+            { prop: "fUcamintime", label: "发生时间" }
+          ]
+        },
+        { prop: "fUcaavg", label: "平均值" }
+      ]
+    }
+  ],
+  P: [
+    { prop: "objectname", label: "回路名称" },
+    { prop: "collecttime", label: "日期", width: 200 },
+    {
+      label: "有功功率(kW)",
+      children: [
+        {
+          label: "最大值",
+          children: [
+            { prop: "text", label: "数值" },
+            { prop: "3sdf11", label: "发生时间" }
+          ]
+        },
+        {
+          label: "最小值",
+          children: [
+            { prop: "text", label: "数值" },
+            { prop: "3sdf11", label: "发生时间" }
+          ]
+        },
+        { prop: "3sdf", label: "平均值" }
+      ]
+    },
+    {
+      label: "无功功率(kVar)",
+      children: [
+        {
+          label: "最大值",
+          children: [
+            { prop: "text", label: "数值" },
+            { prop: "3sdf11", label: "发生时间" }
+          ]
+        },
+        {
+          label: "最小值",
+          children: [
+            { prop: "text", label: "数值" },
+            { prop: "3sdf11", label: "发生时间" }
+          ]
+        },
+        { prop: "3sdf", label: "平均值" }
+      ]
+    },
+    {
+      label: "视在功率(kVA)",
+      children: [
+        {
+          label: "最大值",
+          children: [
+            { prop: "text", label: "数值" },
+            { prop: "3sdf11", label: "发生时间" }
+          ]
+        },
+        {
+          label: "最小值",
+          children: [
+            { prop: "text", label: "数值" },
+            { prop: "3sdf11", label: "发生时间" }
+          ]
+        },
+        { prop: "3sdf", label: "平均值" }
+      ]
+    },
+    {
+      label: "功率因数",
+      children: [
+        {
+          label: "最大值",
+          children: [
+            { prop: "text", label: "数值" },
+            { prop: "3sdf11", label: "发生时间" }
+          ]
+        },
+        {
+          label: "最小值",
+          children: [
+            { prop: "text", label: "数值" },
+            { prop: "3sdf11", label: "发生时间" }
+          ]
+        },
+        { prop: "3sdf", label: "平均值" }
+      ]
+    }
+  ],
+  UnB: [
+    { prop: "objectname", label: "回路名称" },
+    { prop: "collecttime", label: "日期", width: 200 },
+    {
+      label: "电压三相不平衡度(%)",
+      children: [
+        {
+          label: "最大值",
+          children: [
+            { prop: "fUUnBmaxvalue", label: "数值" },
+            { prop: "fUUnBmaxtime", label: "发生时间" }
+          ]
+        },
+        {
+          label: "最小值",
+          children: [
+            { prop: "fUUnBminvalue", label: "数值" },
+            { prop: "fUUnBmintime", label: "发生时间" }
+          ]
+        },
+        { prop: "fUUnBavg", label: "平均值" }
+      ]
+    },
+    {
+      label: "电流三相不平衡度(%)",
+      children: [
+        {
+          label: "最大值",
+          children: [
+            { prop: "fIUnBmaxvalue", label: "数值" },
+            { prop: "fIUnBmaxtime", label: "发生时间" }
+          ]
+        },
+        {
+          label: "最小值",
+          children: [
+            { prop: "fIUnBminvalue", label: "数值" },
+            { prop: "fIUnBmintime", label: "发生时间" }
+          ]
+        },
+        { prop: "fIUnBavg", label: "平均值" }
+      ]
+    }
+  ],
+  UHR: [
+    { prop: "objectname", label: "回路名称" },
+    { prop: "collecttime", label: "日期", width: 200 },
+    {
+      label: "A相电压总谐波含有率(%)",
+      children: [
+        {
+          label: "最大值",
+          children: [
+            { prop: "fUaTHDmaxvalue", label: "数值" },
+            { prop: "fUaTHDmaxtime", label: "发生时间" }
+          ]
+        },
+        {
+          label: "最小值",
+          children: [
+            { prop: "fUaTHDminvalue", label: "数值" },
+            { prop: "fUaTHDmintime", label: "发生时间" }
+          ]
+        },
+        { prop: "fUaTHDavg", label: "平均值" }
+      ]
+    },
+    {
+      label: "B相电压总谐波含有率(%)",
+      children: [
+        {
+          label: "最大值",
+          children: [
+            { prop: "fUbTHDmaxvalue", label: "数值" },
+            { prop: "fUbTHDmaxtime", label: "发生时间" }
+          ]
+        },
+        {
+          label: "最小值",
+          children: [
+            { prop: "fUbTHDminvalue", label: "数值" },
+            { prop: "fUbTHDmintime", label: "发生时间" }
+          ]
+        },
+        { prop: "fUbTHDavg", label: "平均值" }
+      ]
+    },
+    {
+      label: "C相电压总谐波含有率(%)",
+      children: [
+        {
+          label: "最大值",
+          children: [
+            { prop: "fUcTHDmaxvalue", label: "数值" },
+            { prop: "fUcTHDmaxtime", label: "发生时间" }
+          ]
+        },
+        {
+          label: "最小值",
+          children: [
+            { prop: "fUcTHDminvalue", label: "数值" },
+            { prop: "fUcTHDmintime", label: "发生时间" }
+          ]
+        },
+        { prop: "fUcTHDavg", label: "平均值" }
+      ]
+    }
+  ],
+  IHR: [
+    { prop: "objectname", label: "回路名称" },
+    { prop: "collecttime", label: "日期", width: 200 },
+    {
+      label: "A相电流总谐波含有率(%)",
+      children: [
+        {
+          label: "最大值",
+          children: [
+            { prop: "fIaTHDmaxvalue", label: "数值" },
+            { prop: "fIaTHDmaxtime", label: "发生时间" }
+          ]
+        },
+        {
+          label: "最小值",
+          children: [
+            { prop: "fIaTHDminvalue", label: "数值" },
+            { prop: "fIaTHDmintime", label: "发生时间" }
+          ]
+        },
+        { prop: "fIaTHDavg", label: "平均值" }
+      ]
+    },
+    {
+      label: "B相电流总谐波含有率(%)",
+      children: [
+        {
+          label: "最大值",
+          children: [
+            { prop: "fIbTHDmaxvalue", label: "数值" },
+            { prop: "fIbTHDmaxtime", label: "发生时间" }
+          ]
+        },
+        {
+          label: "最小值",
+          children: [
+            { prop: "fIbTHDminvalue", label: "数值" },
+            { prop: "fIbTHDmintime", label: "发生时间" }
+          ]
+        },
+        { prop: "fIbTHDavg", label: "平均值" }
+      ]
+    },
+    {
+      label: "C相电流总谐波含有率(%)",
+      children: [
+        {
+          label: "最大值",
+          children: [
+            { prop: "fIcTHDmaxvalue", label: "数值" },
+            { prop: "fIcTHDmaxtime", label: "发生时间" }
+          ]
+        },
+        {
+          label: "最小值",
+          children: [
+            { prop: "fIcTHDminvalue", label: "数值" },
+            { prop: "fIcTHDmintime", label: "发生时间" }
+          ]
+        },
+        { prop: "fIcTHDavg", label: "平均值" }
+      ]
+    }
+  ]
+};
+
+export { columns };
--- src/views/ElectricExtremumReport/index.vue
+++ src/views/ElectricExtremumReport/index.vue
@@ -12,161 +12,110 @@
               <el-option label="自定义" value="custom" />
             </el-select>
           </el-form-item>
-          <el-form-item label="日期">
-            <el-date-picker v-model="formInline.starttime" type="date" />
+          <el-form-item v-if="formInline.scheme !== 'custom'" label="日期">
+            <el-date-picker
+              v-if="formInline.scheme === 'day'"
+              @change="changeStartTime"
+              v-model="formInline.starttime"
+              type="date"
+            />
+            <el-date-picker
+              v-if="formInline.scheme === 'month'"
+              @change="changeStartTime"
+              v-model="formInline.starttime"
+              type="month"
+            />
+          </el-form-item>
+          <el-form-item v-else label="日期">
+            <el-date-picker v-model="formInline.daterange" @change="changeDaterange" type="daterange" />
           </el-form-item>
           <el-form-item label="电力类别">
             <el-select v-model="formInline.param">
               <el-option label="功率" value="P" />
               <el-option label="电流" value="I" />
-              <el-option label="电压" value="U" />
+              <el-option label="相电压" value="U" />
+              <el-option label="线电压" value="ABCU" />
               <el-option label="不平衡度" value="UnB" />
               <el-option label="电压谐波" value="UHR" />
               <el-option label="电流谐波" value="IHR" />
             </el-select>
           </el-form-item>
           <el-form-item>
-            <el-button-group>
-              <el-button>
+            <el-button-group v-if="formInline.scheme === 'month'" type="primary">
+              <el-button @click="clickPrev">
+                <el-icon class="el-icon--left"><ArrowLeft /></el-icon>上一月
+              </el-button>
+              <el-button @click="clickNext">
+                下一月<el-icon class="el-icon--right"><ArrowRight /></el-icon>
+              </el-button>
+            </el-button-group>
+            <el-button-group v-if="formInline.scheme === 'day'" type="primary">
+              <el-button @click="clickPrev">
                 <el-icon class="el-icon--left"><ArrowLeft /></el-icon>上一日
               </el-button>
-              <el-button>
+              <el-button @click="clickNext">
                 下一日<el-icon class="el-icon--right"><ArrowRight /></el-icon>
               </el-button>
             </el-button-group>
           </el-form-item>
           <el-form-item>
             <el-button type="primary" @click="onSubmit">查询</el-button>
-            <el-button type="primary">导出</el-button>
+            <el-button>导出</el-button>
           </el-form-item>
         </el-form>
-        <PaginationTable :span-method="objectSpanMethod" :columns="columns" :fetch-data="fetchData"> </PaginationTable>
+        <PaginationTable ref="tableRef" :columns="columns[formInline.param]" :fetch-data="fetchData"> </PaginationTable>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="tsx" name="bing">
-import { reactive } from "vue";
-// import moment from "moment";
-import PaginationTable, { SpanMethodProps } from "@/components/PaginationTable/index.vue";
+import { ref, reactive } from "vue";
+import moment from "moment";
+import PaginationTable from "@/components/PaginationTable/index.vue";
 import TransformerSelect from "@/components/TransformerSelect/index.vue";
 import { ElecMaxMinAvgValue } from "@/api/modules/main";
-// import data from "./data.json";
+import { columns } from "./config";
+
+const end = new Date();
+const start = new Date();
+start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+
+const tableRef = ref<any>(null);
 
 const formInline = reactive<{
-  param: "I" | "U" | "P" | "UnB" | "UHR" | "IHR";
+  param: "I" | "U" | "ABCU" | "P" | "UnB" | "UHR" | "IHR";
   scheme: "day" | "month" | "custom";
   starttime: string;
+  daterange: any;
 }>({
   param: "I",
   scheme: "day",
-  starttime: "2024-06-01"
+  starttime: "2024-06-01",
+  daterange: [start, end]
 });
 
-const objectSpanMethod = ({ rowIndex, columnIndex }: SpanMethodProps) => {
-  if (columnIndex === 0) {
-    if (rowIndex % 2 === 0) {
-      return {
-        rowspan: 2,
-        colspan: 1
-      };
-    } else {
-      return {
-        rowspan: 0,
-        colspan: 0
-      };
-    }
-  }
+const onSubmit = () => {
+  tableRef?.value?.resetData();
 };
 
-const columns = [
-  { prop: "objectname", label: "回路名称" },
-  { prop: "collecttime", label: "日期" },
-  {
-    label: "有功功率(kW)",
-    children: [
-      {
-        label: "最大值",
-        children: [
-          { prop: "text", label: "数值" },
-          { prop: "3sdf11", label: "发生时间" }
-        ]
-      },
-      {
-        label: "最小值",
-        children: [
-          { prop: "text", label: "数值" },
-          { prop: "3sdf11", label: "发生时间" }
-        ]
-      },
-      { prop: "3sdf", label: "平均值" }
-    ]
-  },
-  {
-    label: "无功功率(kVar)",
-    children: [
-      {
-        label: "最大值",
-        children: [
-          { prop: "text", label: "数值" },
-          { prop: "3sdf11", label: "发生时间" }
-        ]
-      },
-      {
-        label: "最小值",
-        children: [
-          { prop: "text", label: "数值" },
-          { prop: "3sdf11", label: "发生时间" }
-        ]
-      },
-      { prop: "3sdf", label: "平均值" }
-    ]
-  },
-  {
-    label: "视在功率(kVA)",
-    children: [
-      {
-        label: "最大值",
-        children: [
-          { prop: "text", label: "数值" },
-          { prop: "3sdf11", label: "发生时间" }
-        ]
-      },
-      {
-        label: "最小值",
-        children: [
-          { prop: "text", label: "数值" },
-          { prop: "3sdf11", label: "发生时间" }
-        ]
-      },
-      { prop: "3sdf", label: "平均值" }
-    ]
-  },
-  {
-    label: "功率因数",
-    children: [
-      {
-        label: "最大值",
-        children: [
-          { prop: "text", label: "数值" },
-          { prop: "3sdf11", label: "发生时间" }
-        ]
-      },
-      {
-        label: "最小值",
-        children: [
-          { prop: "text", label: "数值" },
-          { prop: "3sdf11", label: "发生时间" }
-        ]
-      },
-      { prop: "3sdf", label: "平均值" }
-    ]
-  }
-];
+const changeStartTime = value => {
+  if (formInline.scheme === "day") formInline.starttime = moment(value).format("YYYY-MM-DD");
+  if (formInline.scheme === "month") formInline.starttime = moment(value).format("YYYY-MM");
+};
 
-const onSubmit = () => {
-  console.log("submit!");
+const changeDaterange = value => {
+  console.log("changeDaterange", value);
+};
+
+const clickPrev = () => {
+  if (formInline.scheme === "month") formInline.starttime = moment(formInline.starttime).subtract(1, "M").format("YYYY-MM");
+  else formInline.starttime = moment(formInline.starttime).subtract(1, "d").format("YYYY-MM-DD");
+};
+
+const clickNext = () => {
+  if (formInline.scheme === "month") formInline.starttime = moment(formInline.starttime).add(1, "M").format("YYYY-MM");
+  else formInline.starttime = moment(formInline.starttime).add(1, "d").format("YYYY-MM-DD");
 };
 
 const fetchData = async (): Promise<any> => {
@@ -176,8 +125,14 @@ const fetchData = async (): Promise<any> => {
       circuitids: "000-001-002",
       scheme: formInline.scheme,
       starttime: formInline.starttime,
-      param: "I"
+      param: formInline.param === "ABCU" ? "U" : formInline.param
     };
+    if (formInline.scheme === "custom") {
+      params.starttime = moment(formInline.daterange[0]).format("YYYY-MM-DD");
+      params.endtime = moment(formInline.daterange[1]).format("YYYY-MM-DD");
+    } else {
+      params.starttime = formInline.starttime;
+    }
     const { data } = await ElecMaxMinAvgValue(params);
     const list = data?.StatisticValue || [];
     resolve({ list, total: 0 });
