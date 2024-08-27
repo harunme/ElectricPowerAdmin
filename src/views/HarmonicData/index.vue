77d6d675d5bb7faceef135190141dd2a0fb1fce6
harunme <yowangbin@gmail.com>
2024-08-27 20:58:22 +0800

    feat: update

--- src/views/HarmonicData/index.vue
+++ src/views/HarmonicData/index.vue
@@ -25,37 +25,37 @@
           </el-form-item>
           <el-form-item label="选择">
             <el-select v-model="formInline.thdtype" multiple collapse-tags collapse-tags-tooltip :max-collapse-tags="1" clearable>
-              <el-option label="总谐波" value="fThd" />
-              <el-option label="2次谐波含量" value="fHr02" />
-              <el-option label="3次谐波含量" value="fHr03" />
-              <el-option label="4次谐波含量" value="fHr04" />
-              <el-option label="5次谐波含量" value="fHr05" />
-              <el-option label="6次谐波含量" value="fHr06" />
-              <el-option label="7次谐波含量" value="fHr07" />
-              <el-option label="8次谐波含量" value="fHr08" />
-              <el-option label="9次谐波含量" value="fHr09" />
-              <el-option label="10次谐波含量" value="fHr10" />
-              <el-option label="11次谐波含量" value="fHr11" />
-              <el-option label="12次谐波含量" value="fHr12" />
-              <el-option label="13次谐波含量" value="fHr13" />
-              <el-option label="14次谐波含量" value="fHr14" />
-              <el-option label="15次谐波含量" value="fHr15" />
-              <el-option label="16次谐波含量" value="fHr16" />
-              <el-option label="17次谐波含量" value="fHr17" />
-              <el-option label="18次谐波含量" value="fHr18" />
-              <el-option label="19次谐波含量" value="fHr19" />
-              <el-option label="20次谐波含量" value="fHr20" />
-              <el-option label="21次谐波含量" value="fHr21" />
-              <el-option label="22次谐波含量" value="fHr22" />
-              <el-option label="23次谐波含量" value="fHr23" />
-              <el-option label="24次谐波含量" value="fHr24" />
-              <el-option label="25次谐波含量" value="fHr25" />
-              <el-option label="26次谐波含量" value="fHr26" />
-              <el-option label="27次谐波含量" value="fHr27" />
-              <el-option label="28次谐波含量" value="fHr28" />
-              <el-option label="29次谐波含量" value="fHr29" />
-              <el-option label="30次谐波含量" value="fHr30" />
-              <el-option label="31次谐波含量" value="fHr31" />
+              <el-option label="总谐波" value="Thd" />
+              <el-option label="2次谐波含量" value="Hr02" />
+              <el-option label="3次谐波含量" value="Hr03" />
+              <el-option label="4次谐波含量" value="Hr04" />
+              <el-option label="5次谐波含量" value="Hr05" />
+              <el-option label="6次谐波含量" value="Hr06" />
+              <el-option label="7次谐波含量" value="Hr07" />
+              <el-option label="8次谐波含量" value="Hr08" />
+              <el-option label="9次谐波含量" value="Hr09" />
+              <el-option label="10次谐波含量" value="Hr10" />
+              <el-option label="11次谐波含量" value="Hr11" />
+              <el-option label="12次谐波含量" value="Hr12" />
+              <el-option label="13次谐波含量" value="Hr13" />
+              <el-option label="14次谐波含量" value="Hr14" />
+              <el-option label="15次谐波含量" value="Hr15" />
+              <el-option label="16次谐波含量" value="Hr16" />
+              <el-option label="17次谐波含量" value="Hr17" />
+              <el-option label="18次谐波含量" value="Hr18" />
+              <el-option label="19次谐波含量" value="Hr19" />
+              <el-option label="20次谐波含量" value="Hr20" />
+              <el-option label="21次谐波含量" value="Hr21" />
+              <el-option label="22次谐波含量" value="Hr22" />
+              <el-option label="23次谐波含量" value="Hr23" />
+              <el-option label="24次谐波含量" value="Hr24" />
+              <el-option label="25次谐波含量" value="Hr25" />
+              <el-option label="26次谐波含量" value="Hr26" />
+              <el-option label="27次谐波含量" value="Hr27" />
+              <el-option label="28次谐波含量" value="Hr28" />
+              <el-option label="29次谐波含量" value="Hr29" />
+              <el-option label="30次谐波含量" value="Hr30" />
+              <el-option label="31次谐波含量" value="Hr31" />
             </el-select>
           </el-form-item>
           <el-form-item>
@@ -100,7 +100,7 @@ const columns = [
 const formInline = reactive({
   energyKind: "I" as "I" | "U",
   time: "2024-06-01",
-  thdtype: ["fThd"],
+  thdtype: ["Thd"],
   phase: ["a", "b", "c"]
 });
 
@@ -118,11 +118,6 @@ const fetchData = async (): Promise<any> => {
     resolve({ list: data?.PowerValue });
   });
 };
-
-// onMounted(async () => {
-//   const res = await getCircuitInfoTree();
-//   tree.value = res?.data;
-// });
 
 const option: ECOption = {
   title: {
